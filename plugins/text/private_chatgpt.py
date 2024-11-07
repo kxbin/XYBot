cd8d590abea1597eb61df737056c2e073f1e67cb
Jabin Kong <1059978534@qq.com>
2024-11-07 10:36:06 +0800

    优化人员加白指令

--- plugins/text/private_chatgpt.py
+++ plugins/text/private_chatgpt.py
@@ -130,7 +130,7 @@ class private_chatgpt(PluginInterface):
                     json.dump(self.white_group, f, ensure_ascii=False, indent=4)
                 bot.send_text("收到，我会的", recv.roomid)
                 return  # 如果是群里加白指令
-            elif "大家好，我是亿速云" in msg:
+            elif "大家好，我是亿速云" in msg or re.match(r"^#\d+", msg):
                 self.white_people[recv.sender] = msg.replace("大家好，我是亿速云", "")
                 with open('white_people.json', 'w', encoding='utf-8') as f:
                     json.dump(self.white_people, f, ensure_ascii=False, indent=4)
